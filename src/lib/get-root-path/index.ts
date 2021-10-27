6bec61868dc863f8675ac1c55abb1b81661dd867
Henrique Leite <henriqueleite616@gmail.com>
2021-10-27 12:24:03 -0300

    Add isPackageInstalled (#25)

--- src/lib/get-root-path/index.ts
+++ src/lib/get-root-path/index.ts
@@ -1,10 +1,14 @@
-import { resolve } from "path";
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 
 export const LEADING_BAR = /^\//;
 /**
  * Get the root path of the process
  *
- * Extremely useful to create CLIs
+ * Extremely useful to create CLIs and Libs
  */
-export const getRootPath = (path: string) =>
-	resolve(process.cwd(), path.replace(LEADING_BAR, ""));
+export const getRootPath = (path: string) => {
+	const { resolve } = require("path");
+
+	return resolve(process.cwd(), path.replace(LEADING_BAR, ""));
+};
---
+++ src/tests/is-package-installed.spec.ts
@@ -0,0 +1,5 @@
+describe("isPackageInstalled", () => {
+	it.todo(
+		"it uses only node features and other functions, so this cannot be truly tested",
+	);
+});
